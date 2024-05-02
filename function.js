1939ba41ca0ccd4fa880b2f92e7d3dfe465742fc
mahidaathar10 <160022747+mahidaathar10@users.noreply.github.com>
2024-05-02 22:53:42 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,13 @@
+"use strict";
+function studentInformation(Name = "sania", Age = 20, Qualification = "intermediate") {
+    console.log(`Student name is ${Name}`);
+    console.log(`student age is${Age}`);
+    console.log(`student qualification is${Qualification}`);
+}
+studentInformation();
+function carDetails(Name = "toyota corolla", colour = "red", model = 2015) {
+    console.log(`car name${Name}`);
+    console.log(`car colour${colour}`);
+    console.log(`car model${model}`);
+}
+carDetails("toyota corolla", "red", 2015);
---
+++ function.ts
@@ -0,0 +1,14 @@
+function studentInformation(Name:string="sania",Age:number=20,Qualification:string="intermediate"){
+  console.log(`Student name is ${Name}`);
+  console.log(`student age is${Age}`);
+  console.log(`student qualification is${Qualification}`)
+}
+studentInformation()
+ function carDetails(Name:string="toyota corolla",colour:string="red",model:number=2015){
+  console.log(`car name${Name}`);
+  console.log(`car colour${colour}`);
+console.log(`car model${model}`);
+}
+carDetails("toyota corolla","red",2015)
+
+
